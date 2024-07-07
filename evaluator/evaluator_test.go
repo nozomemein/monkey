106bb82223c35f537a568edb1c8598436aa29ba9
nozomemein <b8yukifsukeo999n@gmail.com>
2024-07-07 16:56:32 +0900

    add puts fn

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -165,10 +165,10 @@ func TestErrorHandling(t *testing.T) {
 			`"Hello" - "World"`,
 			"unknown operator: STRING - STRING",
 		},
-    {
-    `{"name": "Monkey"}[fn(x) { x }];`,
-      "unusable as hash key: FUNCTION",
-    },
+		{
+			`{"name": "Monkey"}[fn(x) { x }];`,
+			"unusable as hash key: FUNCTION",
+		},
 	}
 
 	for _, tt := range tests {
