92d73c82ecd8e5f440433b66de5e88655d2db4f7
nozomemein <b8yukifsukeo999n@gmail.com>
2024-07-07 10:27:20 +0900

    evalの雛形

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -41,39 +41,39 @@ func TestLetStatements(t *testing.T) {
 }
 
 func TestReturnStatements(t *testing.T) {
-  tests := []struct {
-    input         string
-    expectedValue interface{}
-  }{
-    {"return 5;", 5},
-    {"return true;", true},
-    {"return foobar;", "foobar"},
-  }
+	tests := []struct {
+		input         string
+		expectedValue interface{}
+	}{
+		{"return 5;", 5},
+		{"return true;", true},
+		{"return foobar;", "foobar"},
+	}
 
-  for _, tt := range tests {
-    l := lexer.New(tt.input)
-    p := New(l)
-    program := p.ParseProgram()
-    checkParserErrors(t, p)
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
 
-    if len(program.Statements) != 1 {
-      t.Fatalf("program.Statements does not contain 1 statements. got=%d", len(program.Statements))
-    }
+		if len(program.Statements) != 1 {
+			t.Fatalf("program.Statements does not contain 1 statements. got=%d", len(program.Statements))
+		}
 
-    stmt := program.Statements[0]
-    returnStmt, ok := stmt.(*ast.ReturnStatement)
-    if !ok {
-      t.Fatalf("stmt not *ast.ReturnStatement. got=%T", stmt)
-    }
+		stmt := program.Statements[0]
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Fatalf("stmt not *ast.ReturnStatement. got=%T", stmt)
+		}
 
-    if returnStmt.TokenLiteral() != "return" {
-      t.Fatalf("returnStmt.TokenLiteral not 'return', got %q", returnStmt.TokenLiteral())
-    }
+		if returnStmt.TokenLiteral() != "return" {
+			t.Fatalf("returnStmt.TokenLiteral not 'return', got %q", returnStmt.TokenLiteral())
+		}
 
-    if !testLiteralExpression(t, returnStmt.ReturnValue, tt.expectedValue) {
-      return
-    }
-  }
+		if !testLiteralExpression(t, returnStmt.ReturnValue, tt.expectedValue) {
+			return
+		}
+	}
 }
 
 func TestIdentifierExpression(t *testing.T) {
